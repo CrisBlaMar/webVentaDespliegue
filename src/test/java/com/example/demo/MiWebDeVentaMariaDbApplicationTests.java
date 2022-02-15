3f258b8a69d3c3e6ca799cea9c812f366b9d9520
Cristina Blanco <74186781+CrisBlaMar@users.noreply.github.com>
2022-02-15 10:43:10 +0100

    Update MiWebDeVentaMariaDbApplicationTests.java

--- src/test/java/com/example/demo/MiWebDeVentaMariaDbApplicationTests.java
+++ src/test/java/com/example/demo/MiWebDeVentaMariaDbApplicationTests.java
@@ -6,8 +6,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MiWebDeVentaMariaDbApplicationTests {
 
+	private Usuario usuario;
+	
+	@BeforeEach
+	public void init() {
+		this.usuario = new Usuario("crisblamar");
+	}
+	
 	@Test
-	void contextLoads() {
+	void testGetUserName() {
+		assertEquals("crisblamar", usuario.getUserName());
 	}
 
 }
