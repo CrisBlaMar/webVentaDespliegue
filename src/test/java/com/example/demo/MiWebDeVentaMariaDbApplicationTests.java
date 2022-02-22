a06aaf7251b8fa3d1626c8c50c1b981e7364f82f
Cristina Blanco <74186781+CrisBlaMar@users.noreply.github.com>
2022-02-22 12:01:15 +0100

    Update MiWebDeVentaMariaDbApplicationTests.java

--- src/test/java/com/example/demo/MiWebDeVentaMariaDbApplicationTests.java
+++ src/test/java/com/example/demo/MiWebDeVentaMariaDbApplicationTests.java
@@ -19,11 +19,7 @@ class MiWebDeVentaMariaDbApplicationTests {
 	
 	@Test
 	void testGetUserName() {
-<<<<<<< HEAD
 		assertEquals("crisblamar", usuario.getUsuario());
-=======
-		assertEquals("ruieh", usuario.getUsuario());
->>>>>>> 0f415f313e53cf6b1d3b9f38a60158037bf9d2d2
 	}
 
 
